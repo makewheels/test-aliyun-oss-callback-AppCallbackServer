e2e0854345203865cb757ab1f485e3ebe853985b
bq <gOtQWGG4wZ>
2021-02-02 17:25:28 +0800

    https://github.com/makewheels/test-aliyun-oss-callback-AppCallbackServer.git

--- src/main/java/com/demo/demo/App.java
+++ src/main/java/com/demo/demo/App.java
@@ -10,35 +10,27 @@ import org.eclipse.jetty.server.session.SessionHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+public class App {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server();
+        int port = 7090;//默认端口是：7090
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args ) throws Exception
-    {
-    	Server server = new Server();
-    	int port = 7090;//默认端口是：7090
-    	if (args.length > 0)
-    	{
-    		port = Integer.parseInt(args[0]);
-    	}
-    	
-    	ServerConnector connector = new ServerConnector(server);
+        ServerConnector connector = new ServerConnector(server);
         connector.setPort(port);
         System.out.println("server run on port:" + port);
-        
-        server.setConnectors(new Connector[] { connector });
+
+        server.setConnectors(new Connector[]{connector});
         //设置handler根路径
         ServletContextHandler webApiContext = new ServletContextHandler();
         webApiContext.setContextPath("/");
-        webApiContext.addServlet( new ServletHolder(CallbackServer.class), "/*");
+        webApiContext.addServlet(new ServletHolder(CallbackServer.class), "/*");
         webApiContext.setSessionHandler(new SessionHandler());
-        
+
         HandlerList handlers = new HandlerList();
-        handlers.setHandlers(new Handler[] { webApiContext, new DefaultHandler() });
+        handlers.setHandlers(new Handler[]{webApiContext, new DefaultHandler()});
         //增加handlers并启动server
         server.setHandler(handlers);
         server.start();
